d88cb0e165ff5b2a96b308dc654cb3c629e2e5df
Srava2000 <126134820+Srava2000@users.noreply.github.com>
2025-07-17 03:49:22 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -35,6 +35,7 @@
 
 import "./globals.css";
 import { Toaster } from "react-hot-toast";
+import Themetoggle from "@/components/Themetoggle";
 
 export default function RootLayout({
   children,
@@ -45,6 +46,7 @@ export default function RootLayout({
     <html lang="en">
       <body>
         <Toaster position="top-right" />
+         <Themetoggle />
         {children}
       </body>
     </html>
